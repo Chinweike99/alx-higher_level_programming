faa81a834a21dc3cb3ffcd239fce24539cfb9e74
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-10-03 15:23:46 -0700

    ASCII Lowercase

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-eAscci = ord('q')
-jAscci = ord('e')
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
 
-for ascci_value in range(97, 123):
-    if ascci_value != eAscci and ascci_value != jAscci:
-        print(chr(ascci_value), end = "")
