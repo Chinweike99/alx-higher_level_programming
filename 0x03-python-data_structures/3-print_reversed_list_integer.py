a87cf77cf2d4114671f7dd3a94d9339b88d24ed4
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-10-09 08:38:13 -0700

    Reverse list

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    return my_list[::-1]
+
+my_list = [1, 2, 3, 4, 5]
+reversed_list = print_reversed_list_integer(my_list)
+for i in reversed_list:
+    print("{}".format(i))
