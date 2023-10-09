359b205843edf40d7715ceafc14498dd5d4b2667
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-10-09 12:46:16 -0700

    Palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*add_nodeint - Function that adds a new node at the beginning of a listint_t list
+*@head: head of listint_t
+*@n: int to add in list
+*Return: address of the new element, or NULL on failure
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *ptr;
+
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->n = n;
+	ptr->next = *head;
+	*head = ptr;
+	return (ptr);
+}
+/**
+*is_palindrome - Function that identifies if a single linked list is palindrome
+*@head: head of listint_t
+*Return: 1 if it is palindrome else 0
+*/
+int is_palindrome(listint_t **head)
+{
+	listint_t *head2 = *head;
+	listint_t *aux = NULL, *aux2 = NULL;
+
+	if (*head == NULL || head2->next == NULL)
+		return (1);
+	while (head2 != NULL)
+	{
+		add_nodeint(&aux, head2->n);
+		head2 = head2->next;
+	}
+	aux2 = aux;
+	while (*head != NULL)
+	{
+		if ((*head)->n != aux2->n)
+		{
+			free_listint(aux);
+			return (0);
+		}
+		*head = (*head)->next;
+		aux2 = aux2->next;
+	}
+	free_listint(aux);
+	return (1);
+}
