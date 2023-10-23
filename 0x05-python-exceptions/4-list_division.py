a5ef28160d45c20b89b1594640b0f61e9a72799f
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-10-23 13:29:25 -0700

    Divide alist

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
-    result = []  # This list will store the division results
-
+    result = [0] * list_length  # Initialize the result list with zeros
+    
     for i in range(list_length):
         try:
             value_1 = my_list_1[i]
             value_2 = my_list_2[i]
+
+            # Check if the elements are numbers (int or float)
             if not (isinstance(value_1, (int, float)) and isinstance(value_2, (int, float))):
                 print("wrong type")
-                result.append(0)
-                continue
-            if value_2 == 0:
-                print("division by 0")
-                result.append(0)
             else:
-                result.append(value_1 / value_2)
+                # Check if the division can be done (no division by zero)
+                if value_2 == 0:
+                    print("division by 0")
+                else:
+                    result[i] = value_1 / value_2
         except IndexError:
             print("out of range")
-            result.append(0)
-
     return result
