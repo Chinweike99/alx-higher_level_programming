e53c5a19a1039cf0b47eec5935f665ee0ba2d62a
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-10-25 08:03:01 -0700

    An empty Class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """
-    An empty class that defines a square.
-
-    pass:
-        This entails ignore and continue
-    """
+    """An empty class that defines a square."""
     pass
