b5270efbe8231e99d6aa06a3eab1aefa1b20ce66
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-10-25 08:18:21 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is the Square module."""
+
+
+class Square:
+    """Class of an empty square with reference to (0-sqaure.py)"""
+
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length of side of the square.
+        """
+        self.__size = size
