8157bdb6e94ea1f310638516c8e83f7cdf25bd29
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-11-06 15:42:12 -0800

    Inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,6 +4,9 @@
 def lookup(obj):
     """A function that returns the list of available attributes
         and method of an object.
+    Args:
+        obj - Object of the class
+
     Return:
         a list object
     """
