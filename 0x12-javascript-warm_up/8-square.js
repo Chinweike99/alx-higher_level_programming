9eb2f68ffacffee1086743f49606447f12f57b12
Chinweike99 <Innocentakwolu1@gmail.com>
2024-01-09 07:25:45 -0800

    updated

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
-
-const size = parseInt(process.argv[2]);
-if (!isNaN(size)) {
-	for (let i = 0; i < size; i++) {
-		let row = "";
-		for (let k = 0; k < size; k++) {
-			row += "X";
-		}
-		console.log(row);
-	}
-} else {
-	console.log("Missing size");
+function factorial (nbr) {
+  if (!nbr || nbr < 2) {
+    return (1);
+  }
+  return nbr * factorial(nbr - 1);
 }
+console.log(factorial(Number(process.argv[2])));
